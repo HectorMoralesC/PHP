5a27a020064396470e085bcd218109ff1880bc5c
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-06-12 17:30:21 +0200

    Exámen Héctor 12-6-23

--- vista.php
+++ vista.php
@@ -2,9 +2,29 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Formulari de Contacte</title>
+
 </head>
 <body>
-	<h1>El resultado del área del hexágono es: <?php include 'index1.php'; ?></h1>
+	<div class="wrap">
+	
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+			<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre:" value="<?php if(!$enviado && isset($nombre)) echo $nombre ?>">
+
+			<input type="text" class="form-control" id="correo" name="correo" placeholder="Correo:" value="<?php if(!$enviado && isset($correo)) echo $correo ?>">
+
+			<?php if (!empty($errores)): ?>
+				<div class="alert error">
+					<?php echo $errores; ?>
+				</div>
+			<?php elseif($enviado): ?>
+				<div class="alert success">
+					<p>Enviado Correctamente</p>
+				</div>
+			<?php endif ?>
+
+			<input type="submit" name="submit" class="btn btn-primary" value="Enviar Correo">
+		</form>
+	</div>
 </body>
 </html>
