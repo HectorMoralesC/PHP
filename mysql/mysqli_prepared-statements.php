4bcafd1ac88281d518847e881403296d4d06c50e
HectorMoralesC <hectormoralescortes6@gmail.com>
2023-07-10 19:21:03 +0200

    10-7-23

--- mysql/mysqli_prepared-statements.php
+++ mysql/mysqli_prepared-statements.php
@@ -1,28 +1,29 @@
 <?php
-$conexion = new mysqli('localhost', 'root', '', 'prueba_datos');
+$conexion = new mysqli('localhost', 'root', '', 'prueba_consola');
 
 if ($conexion->connect_errno){
 	die('Lo siento hubo un problema con el servidor');
 } else {
-	$statement = $conexion->prepare("INSERT INTO usuarios(ID, nombre, edad) VALUES(?, ?, ?)");
+	$statement = $conexion->prepare("INSERT INTO usuarios(id, nombre, email, edad) VALUES(?, ?, ?, ?)");
 	
 	// Remplazamos los placeholder ? con los valores que queremos usar.
 		// Una S por placeholder que tengamos.
 		// s = string
 		// i = integer
 		// d = double
-	$statement->bind_param('ssi', $id, $nombre, $edad);
+	$statement->bind_param('sssi', $id, $nombre, $email, $edad);
 	$id = NULL;
 
 	// Comprobamos que hayamos pasado un nombre por URL
-	if(isset($_GET['nombre']) && isset($_GET['edad'])){
+	if(isset($_GET['nombre']) && isset($_GET['email']) && isset($_GET['edad'])){
 		$nombre = $_GET['nombre'];
+		$email = $_GET['email'];
 		$edad = $_GET['edad'];
 	}
 
 	// Ejecutamos la sentencia.
 	$statement->execute();
-	// echo 'Filas aÃ±adidas:' . $conexion->affected_rows;
+	// echo 'Filas añadidas:' . $conexion->affected_rows;
 
 	if($conexion->affected_rows >= 1){
 		echo 'Filas agregadas: ' . $conexion->affected_rows;
